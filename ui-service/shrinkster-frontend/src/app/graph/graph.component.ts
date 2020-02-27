33c416d7793c40a0291fab9576b9c858f98946ff
Sagar Jain <sagar@Sagars-MacBook-Air.local>
2020-02-28 01:46:09 +0530

    live graph with websocket

--- ui-service/shrinkster-frontend/src/app/graph/graph.component.ts
+++ ui-service/shrinkster-frontend/src/app/graph/graph.component.ts
@@ -1,43 +1,101 @@
-import { Component, OnInit } from '@angular/core';
-import * as CanvasJS from '../../assets/lib/canvasjs.min';
-
-
+import { Component, OnInit } from "@angular/core";
+import * as CanvasJS from "../../assets/lib/canvasjs.min";
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
 @Component({
-  selector: 'app-graph',
-  templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.css']
+  selector: "app-graph",
+  templateUrl: "./graph.component.html",
+  styleUrls: ["./graph.component.css"]
 })
 export class GraphComponent implements OnInit {
-
-  constructor() { }
-
+    private stompClient = null;
+  name: string;
+  disabled = true;
+  greetings: any;
+  constructor() {}
+  datapoints = [];
+  xValue = 0;
   ngOnInit() {
-    let chart = new CanvasJS.Chart("chartContainer", {
+	  for (let index = 0; index < 10; index++) {
+		this.datapoints.push({x:this.xValue++,y:0});
+	  }
+	let chart = new CanvasJS.Chart("chartContainer", {
 		animationEnabled: true,
 		exportEnabled: true,
-		title: {
-			text: "Basic Column Chart in Angular"
+		axisX:{
+		  crosshair: {
+		  enabled: true,
+		  labelFormatter: e => { return "Hello"}
 		},
-		data: [{
+		gridThickness: 0,
+		tickLength: 0,
+		 labelFormatter: function(){
+			return " ";
+		  }
+	  },axisY:{
+		crosshair: {
+		enabled: true,
+		labelFormatter: e => { return "Hello"}
+	  },
+	  minimum:-0.5,
+	  tickLength: 0,
+	  gridThickness: 0,
+	  labelFormatter: function(){
+		return " ";
+	  }
+	},dataPointWidth: 25,
+		data: [
+		  {
+			color: "#696969",
 			type: "column",
-			dataPoints: [
-				{ y: 71, label: "Apple" },
-				{ y: 55, label: "Mango" },
-				{ y: 50, label: "Orange" },
-				{ y: 65, label: "Banana" },
-				{ y: 95, label: "Pineapple" },
-				{ y: 68, label: "Pears" },
-				{ y: 28, label: "Grapes" },
-				{ y: 34, label: "Lychee" },
-				{ y: 14, label: "Jackfruit" }
-			]
-		}]
+			showInLegend: true,
+			legendMarkerType: "none",
+			legendText: "Registering tiny urls",
+			dataPoints: this.datapoints
+		  }
+		]
+	  });
+	  chart.render();
+	  
+	const socket = new SockJS('/url/ws-graph');
+    this.stompClient = Stomp.over(socket);
+ 
+    const _this = this;
+    this.stompClient.connect({}, function (frame) {
+      _this.setConnected(true);
+      _this.stompClient.subscribe('/topic/greetings', function (hello) {
+		_this.showGreeting(JSON.parse(hello.body));
+		chart.render();
+      });
 	});
-		
-	chart.render();
-   
-
-}
-  
+  }
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+ 
+    if (connected) {
+      this.greetings = [];
+    }
+  }
+  showGreeting(message) {
+	// this.greetings = message.tinyUrl;
+	this.datapoints.push({
+		x: this.xValue++,
+		y: message.tinyUrl
+	  });
+	  if (this.datapoints.length > 20) {
+		this.datapoints.shift();
+	  }
+  }
+ disconnect() {
+    if (this.stompClient != null) {
+      this.stompClient.disconnect();
+    }
+ 
+    this.setConnected(false);
+    console.log('Disconnected!');
+  }
+  ngOnDestroy(){
+      this.disconnect();
+  }
 
 }
--- ui-service/shrinkster-frontend/src/app/home-page/home-page.component.ts
+++ ui-service/shrinkster-frontend/src/app/home-page/home-page.component.ts
@@ -2,18 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginDialogBoxComponent } from '../login-dialog-box/login-dialog-box.component';
 import { MatDialog } from '@angular/material/dialog';
-import * as SockJS from 'sockjs-client';
-import * as Stomp from 'stompjs';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  private stompClient = null;
-  name: string;
-  disabled = true;
-  greetings: any;
   constructor(private router:Router, private dialog:MatDialog){}
   show() {
     if (document.cookie.length > 0) {
@@ -24,43 +19,7 @@ export class HomePageComponent implements OnInit {
         height: "250px"
       });
     }
-  }
-  ngOnInit() {
-    const socket = new SockJS('/url/ws-graph');
-    this.stompClient = Stomp.over(socket);
- 
-    const _this = this;
-    this.stompClient.connect({}, function (frame) {
-      _this.setConnected(true);
-      console.log('Connected: ' + frame);
-      _this.stompClient.subscribe('/topic/greetings', function (hello) {
-        console.log("ddwd");
-        _this.showGreeting(JSON.parse(hello.body));
-        console.log(hello+";;;;;;;");
-      });
-    });
-    
-  }
-  setConnected(connected: boolean) {
-    this.disabled = !connected;
- 
-    if (connected) {
-      this.greetings = [];
-    }
-  }
-  showGreeting(message) {
-    this.greetings = message.urlCount;
-  }
- disconnect() {
-    if (this.stompClient != null) {
-      this.stompClient.disconnect();
-    }
- 
-    this.setConnected(false);
-    console.log('Disconnected!');
-  }
-  ngOnDestroy(){
-      this.disconnect();
-  }
+  } ngOnInit(){
 
+  }
 }
